4ea062ffc0661d4304c6b6e8da21bf0b35fa8db3
Slippnot <jaime.gon100@gmail.com>
2025-07-09 16:22:36 -0700

    bug fixes and optimization

--- index.js
+++ index.js
@@ -20,18 +20,9 @@ function getChoices(){
   let wrong2 = countries[Math.floor(Math.random() * countries.length)];
   let wrong3 = countries[Math.floor(Math.random() * countries.length)];
 
-  if(wrong1 == wrong2 || wrong1 == wrong3){
-    wrong1 = removed[Math.floor(Math.random() * removed.length)];
-    console.log(`wrongCH ${wrong1}`);
-  }
-  if(wrong2 == wrong1 || wrong2 == wrong3){
-    wrong2 = removed[Math.floor(Math.random() * removed.length)];
-    console.log(`wrongCH ${wrong2}`);
-  }
-  if(wrong3 == wrong1 || wrong3 == wrong2){
-    wrong3 = removed[Math.floor(Math.random() * removed.length)];
-    console.log(`wrongCH ${wrong3}`);
-  }
+  preventMultipleWrongAnswerShowing(wrong1,wrong2,wrong3);
+  preventMultipleWrongAnswerShowing(wrong2,wrong1,wrong3);
+  preventMultipleWrongAnswerShowing(wrong3,wrong1,wrong2);
 
   if(removed.includes("This Is Super Rare")){
     removed.shift();
@@ -40,13 +31,21 @@ function getChoices(){
   let flagQuestion = randomFlag;
   let flagAnswer = randomFlag;
 
-  // 1 and 2 are the same, so change etc.
-  twoAnswerShowing(option1,option2,flagAnswer);
-  twoAnswerShowing(option1,option3,flagAnswer);
-  twoAnswerShowing(option1,option4,flagAnswer);
-  twoAnswerShowing(option2,option3,flagAnswer);
-  twoAnswerShowing(option2,option4,flagAnswer);
-  twoAnswerShowing(option3,option4,flagAnswer);
+  console.log(flagAnswer);
+
+  if(countries.length <= 0){
+    optionBTN.forEach((e) => {
+      e.style.display = `none`;
+    });
+    countryFlag.style.display = `none`;
+    scoreDisplay.innerHTML = `${score} / 205 CORRECT | INCORRECT : ${wrong}`;
+    const reset = document.createElement('button');
+    document.body.appendChild(reset);
+    reset.innerHTML = `RESET`;
+    reset.onclick = () => {
+      location.reload();
+    }
+  }
 
   removed.push(`${flagAnswer}`);
 
@@ -57,15 +56,19 @@ function getChoices(){
   let rng = getRandomNum();
   if(rng == 1){
     optionChoices(flagAnswer,wrong1,wrong2,wrong3);
+    preventsTwoCorrectAnswerShowing(flagAnswer);
   }
   if(rng == 2){
     optionChoices(wrong1,flagAnswer,wrong2,wrong3);
+    preventsTwoCorrectAnswerShowing(flagAnswer);
   }
   if(rng == 3){
     optionChoices(wrong1,wrong2,flagAnswer,wrong3);
+    preventsTwoCorrectAnswerShowing(flagAnswer);
   }
   if(rng == 4){
     optionChoices(wrong1,wrong2,wrong3,flagAnswer);
+    preventsTwoCorrectAnswerShowing(flagAnswer);
   }
 
   play(option1,flagAnswer);
@@ -73,48 +76,54 @@ function getChoices(){
   play(option3,flagAnswer);
   play(option4,flagAnswer);
 
-  if(countries.length <= 0){
-    optionBTN.forEach((e) => {
-      e.style.display = `none`;
-    });
-    countryFlag.style.display = `none`;
-    scoreDisplay.innerHTML = `${score} / 204 CORRECT | INCORRECT : ${wrong}`;
-    const reset = document.createElement('button');
-    document.body.appendChild(reset);
-    reset.innerHTML = `RESET`;
-    reset.onclick = () => {
-      location.reload();
-    }
-  }
-
 }
 
 function play(opt,fa){
   opt.onclick = () => {
     if(opt.innerHTML == fa){
       score++;
-      scoreDisplay.innerHTML = `${score} / 204 CORRECT It Was ${fa}`;
+      scoreDisplay.innerHTML = `${score} / 205 CORRECT It Was ${fa}`;
       getChoices();
     }
     else {
       wrong++;
-      scoreDisplay.innerHTML = `${score} / 204 WRONG It Was ${fa}`;
+      scoreDisplay.innerHTML = `${score} / 205 WRONG It Was ${fa}`;
       getChoices();
     }
   }
 }
 
 function optionChoices(opt1,opt2,opt3,opt4){
-    option1.innerHTML = `${opt1}`;
-    option2.innerHTML = `${opt2}`;
-    option3.innerHTML = `${opt3}`;
-    option4.innerHTML = `${opt4}`;
+  option1.innerHTML = `${opt1}`;
+  option2.innerHTML = `${opt2}`;
+  option3.innerHTML = `${opt3}`;
+  option4.innerHTML = `${opt4}`;
 }
 
-function twoAnswerShowing(opt1,opt2,fa){
-  if(opt1.innerHTML == fa && opt2.innerHTML == fa){
-    opt2.innerHTML = removed[Math.floor(Math.random() * removed.length)];
-    console.log(`sameAnswerCH`);
+function preventMultipleWrongAnswerShowing(w1,w2,w3){
+  if(w1 == w2 || w1 == w3){
+    w1 = removed[Math.floor(Math.random() * removed.length)];
+  }
+}
+
+function preventsTwoCorrectAnswerShowing(fa){
+  if(option1.innerHTML == fa && option2.innerHTML == fa){
+    option2.innerHTML = removed[Math.floor(Math.random() * removed.length)];
+  }
+  if(option1.innerHTML == fa && option3.innerHTML == fa){
+    option3.innerHTML = removed[Math.floor(Math.random() * removed.length)];
+  }
+  if(option1.innerHTML == fa && option4.innerHTML == fa){
+    option4.innerHTML = removed[Math.floor(Math.random() * removed.length)];
+  }
+  if(option2.innerHTML == fa && option3.innerHTML == fa){
+    option3.innerHTML = removed[Math.floor(Math.random() * removed.length)];
+  }
+  if(option2.innerHTML == fa && option4.innerHTML == fa){
+    option4.innerHTML = removed[Math.floor(Math.random() * removed.length)];
+  }
+  if(option3.innerHTML == fa && option4.innerHTML == fa){
+    option4.innerHTML = removed[Math.floor(Math.random() * removed.length)];
   }
 }
 
