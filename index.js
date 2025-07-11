aa4fa23d97db069e5ac57b6dc0bc1955be460af8
Slippnot <jaime.gon100@gmail.com>
2025-07-11 16:25:00 -0700

    updated scoring system, added hiscores, updated css, updated showing no duplicates, added end game screen and reset button, updated html, added local save for hiscore

--- index.js
+++ index.js
@@ -7,22 +7,38 @@ const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
 const option4 = document.getElementById("option4");
 const optionBTN = document.querySelectorAll(".optionBTN");
+const resetBtn = document.getElementById("resetBtn");
 const scoreDisplay = document.getElementById("scoreDisplay");
+const hiScoreDisplay = document.getElementById("hiScoreDisplay");
 
 getChoices();
 
 let score = 0;
 let wrong = 0;
+let hiScore = 0;
+
+if(localStorage.getItem("hiScoreSaved") !== null){
+  let savedHiScore = JSON.parse(localStorage.getItem("hiScoreSaved"));
+  hiScoreDisplay.innerHTML = `Hi-Score: ${savedHiScore} / 205`;
+  hiScore = savedHiScore;
+}
 
 function getChoices(){
+  
   let randomFlag = countries[Math.floor(Math.random() * countries.length)];
   let wrong1 = countries[Math.floor(Math.random() * countries.length)];
   let wrong2 = countries[Math.floor(Math.random() * countries.length)];
   let wrong3 = countries[Math.floor(Math.random() * countries.length)];
 
-  preventMultipleWrongAnswerShowing(wrong1,wrong2,wrong3);
-  preventMultipleWrongAnswerShowing(wrong2,wrong1,wrong3);
-  preventMultipleWrongAnswerShowing(wrong3,wrong1,wrong2);
+  if(wrong1 == wrong2 || wrong1 == wrong3){
+    wrong1 = removed[Math.floor(Math.random() * removed.length)];
+  }
+  if(wrong2 == wrong1 || wrong2 == wrong3){
+    wrong2 = removed[Math.floor(Math.random() * removed.length)];
+  }
+  if(wrong3 == wrong1 || wrong3 == wrong2){
+    wrong3 = removed[Math.floor(Math.random() * removed.length)];
+  }
 
   if(removed.includes("This Is Super Rare")){
     removed.shift();
@@ -31,23 +47,7 @@ function getChoices(){
   let flagQuestion = randomFlag;
   let flagAnswer = randomFlag;
 
-  if(countries.length <= 0){
-    optionBTN.forEach((e) => {
-      e.style.display = `none`;
-    });
-    countryFlag.style.display = `none`;
-    scoreDisplay.innerHTML = `${score} / 205 CORRECT | INCORRECT : ${wrong}`;
-    const reset = document.createElement('button');
-    document.body.appendChild(reset);
-    reset.innerHTML = `RESET`;
-    reset.onclick = () => {
-      location.reload();
-    }
-  }
-
-  removed.push(`${flagAnswer}`);
-
-  countries.splice(countries.indexOf(randomFlag), 1);
+  endGameAndReset();
 
   countryFlag.src = `flags/countries/${flagQuestion}.png`;
 
@@ -69,21 +69,30 @@ function getChoices(){
     preventsTwoCorrectAnswerShowing(flagAnswer);
   }
 
-  play(option1,flagAnswer);
-  play(option2,flagAnswer);
-  play(option3,flagAnswer);
-  play(option4,flagAnswer);
+  play(option1,flagAnswer,randomFlag);
+  play(option2,flagAnswer,randomFlag);
+  play(option3,flagAnswer,randomFlag);
+  play(option4,flagAnswer,randomFlag);
 
 }
 
-function play(opt,fa){
+function play(opt,fa,rf){
   opt.onclick = () => {
     if(opt.innerHTML == fa){
+      removed.push(`${fa}`);
+      countries.splice(countries.indexOf(rf), 1);
       score++;
       scoreDisplay.innerHTML = `${score} / 205 CORRECT It Was ${fa}`;
+      if(score > hiScore){
+        hiScore = score;
+        hiScoreDisplay.innerHTML = `Hi-Score: ${hiScore} / 205`;
+        localStorage.setItem("hiScoreSaved", JSON.stringify(hiScore));
+      }
       getChoices();
     }
     else {
+      removed.push(`${fa}`);
+      countries.splice(countries.indexOf(rf), 1);
       wrong++;
       scoreDisplay.innerHTML = `${score} / 205 WRONG It Was ${fa}`;
       getChoices();
@@ -96,12 +105,6 @@ function optionChoices(opt1,opt2,opt3,opt4){
   option2.innerHTML = `${opt2}`;
   option3.innerHTML = `${opt3}`;
   option4.innerHTML = `${opt4}`;
-}
-
-function preventMultipleWrongAnswerShowing(w1,w2,w3){
-  if(w1 == w2 || w1 == w3){
-    w1 = removed[Math.floor(Math.random() * removed.length)];
-  }
 }
 
 function preventsTwoCorrectAnswerShowing(fa){
@@ -122,6 +125,21 @@ function preventsTwoCorrectAnswerShowing(fa){
   }
   if(option3.innerHTML == fa && option4.innerHTML == fa){
     option4.innerHTML = removed[Math.floor(Math.random() * removed.length)];
+  }
+}
+
+function endGameAndReset(){
+  if(countries.length <= 0){
+    optionBTN.forEach((e) => {
+      e.style.display = `none`;
+    });
+    countryFlag.style.display = `none`;
+    hiScoreDisplay.style.display = `block`;
+    scoreDisplay.innerHTML = `${score} / 205 CORRECT | INCORRECT : ${wrong}`;
+    resetBtn.style.display = `block`;
+    resetBtn.onclick = () => {
+      location.reload();
+    }
   }
 }
 
